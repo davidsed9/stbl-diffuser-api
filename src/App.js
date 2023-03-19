a1ad9483e1fef9d242f7ab092bc207cce89cc035
davidsed9 <davidsed9@gmail.com>
2023-03-19 17:24:09 +0000

    Button working perfectly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from './Main'
 import Navbar from './Navbar'
 import SubmitButton from './SubmitButton'
-
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 function App() {
   return (
--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -1,109 +1,39 @@
-import React, { useRef, useEffect } from "react";
-import anime from "animejs/lib/anime.es.js";
+import React, { useRef, useEffect, useState } from "react";
 import "./SubmitButton.css";
+import anime from "animejs/lib/anime.es.js";
+import $ from "jquery";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+library.add(faCheck);
+
 
 function SubmitButton() {
-  const buttonRef = useRef(null);
-  const textRef = useRef(null);
-  const progressBarRef = useRef(null);
-  const checkRef = useRef(null);
+  const [showCheck, setShowCheck] = useState(false);
 
-  useEffect(() => {
-    const basicTimeline = anime.timeline({
-      autoplay: false,
-    });
+  function handleClick() {
+    $("#button").addClass("onclic");
 
-    const pathEls = checkRef.current.querySelectorAll(".check");
-    for (var i = 0; i < pathEls.length; i++) {
-      const pathEl = pathEls[i];
-      const offset = anime.setDashoffset(pathEl);
-      pathEl.setAttribute("stroke-dashoffset", offset);
-    }
+    setTimeout(function () {
+      $("#button").removeClass("onclic");
+      $("#button").addClass("validate");
+      setShowCheck(true)
+    }, 2250);
 
-    basicTimeline
-      .add({
-        targets: textRef.current,
-        duration: 1,
-        opacity: 0,
-      })
-      .add(
-        {
-          targets: buttonRef.current,
-          duration: 1300,
-          height: 10,
-          width: 300,
-          backgroundColor: "#2B2D2F",
-          border: "0",
-          borderRadius: 100,
-        },
-        "-=1"
-      )
-      .add(
-        {
-          targets: progressBarRef.current,
-          duration: 2000,
-          width: 300,
-          easing: "linear",
-        },
-        "-=1300"
-      )
-      .add(
-        {
-          targets: buttonRef.current,
-          width: 0,
-          duration: 1,
-        },
-        "-=2000"
-      )
-      .add(
-        {
-          targets: progressBarRef.current,
-          width: 80,
-          height: 80,
-          delay: 500,
-          duration: 750,
-          borderRadius: 80,
-          backgroundColor: "#71DFBE",
-        },
-        "-=1"
-      )
-      .add(
-        {
-          targets: pathEls,
-          strokeDashoffset: [anime.setDashoffset, 0],
-          duration: 200,
-          easing: "easeInOutSine",
-        },
-        "-=500"
-      );
-
-    buttonRef.current.addEventListener("click", () => {
-      basicTimeline.play();
-    });
-
-    textRef.current.addEventListener("click", () => {
-      basicTimeline.play();
-    });
-  }, []);
+    setTimeout(function () {
+      $("#button").removeClass("validate");
+      setShowCheck(false);
+    }, 4500);
+  }
 
   return (
-    <main>
-      <div className="button" ref={buttonRef}>
-        <div className="text" ref={textRef}>
-          Submit
-        </div>
-      </div>
-      <div className="progress-bar" ref={progressBarRef}></div>
-      <svg
-        x="0px"
-        y="0px"
-        viewBox="0 0 25 30"
-        style={{ enableBackground: "new 0 0 25 30" }}
-        ref={checkRef}
-      >
-        <path className="check" d="M2,19.2C5.9,23.6,9.4,28,9.4,28L23,2" />
-      </svg>
-    </main>
+    <div className="container">
+      <button id="button" onClick={handleClick}>
+      {showCheck && <FontAwesomeIcon icon="check" className="check-icon" />}
+      </button>
+    </div>
   );
 }
 
